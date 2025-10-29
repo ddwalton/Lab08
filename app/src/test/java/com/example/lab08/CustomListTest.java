2f44ba6d1993033829c43d4d703dbf0f3f8395d5
Dion Walton <ddwalton@ualberta.ca>
2025-10-29 17:52:36 -0600

    added countCities test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,8 +1,11 @@
 package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+
     @Test
     public void hasCity() {
         CustomList list = new CustomList();
@@ -20,5 +23,27 @@ public class CustomListTest {
 
         list.deleteCity(calgary);
         assertFalse(list.hasCity(calgary));
+    }
+
+    @Test
+    public void countCities() {
+        CustomList list = new CustomList();
+
+        assertEquals(0, list.countCities());
+
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+
+        assertEquals(2, list.countCities());
+
+        list.deleteCity(calgary);
+
+        assertEquals(1, list.countCities());
+
+        list.deleteCity(edmonton);
+
+        assertEquals(0, list.countCities());
     }
 }
